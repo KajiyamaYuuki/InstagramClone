007f7e4cf60d8e0565f42005f20d4de33d387c94
yuuki kajiyama <luv4u.1331@gmail.com>
2021-02-26 16:04:05 +0900

    feat.:modelの設定追加

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,6 @@
 class Picture < ApplicationRecord
+  validates :content, presence: true
+  mount_uploader :image, ImageUploader
   belongs_to :user
   has_many :favorites
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'favorites/create'
-  get 'favorites/destroy'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :pictures
 end
