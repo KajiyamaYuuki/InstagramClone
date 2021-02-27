a9659774403749d879ae28da9b57465d78b0c451
yuuki kajiyama <luv4u.1331@gmail.com>
2021-02-27 14:52:34 +0900

    feat.:sessionの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,15 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      redirect_to user_path(@user.id), notice: "ユーザーを新規登録しました"
+
     else
+      flash.now[:danger] = "登録に失敗しました"
       render  :new
     end
+  end
+  def show
+    @user = User.find(params[:id])
   end
   private
   def user_params
