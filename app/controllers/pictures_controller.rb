7a60e321136fa647582eaf9aac86d5ff774e68b2
yuuki kajiyama <luv4u.1331@gmail.com>
2021-03-01 18:27:03 +0900

    feat.:プロフィール編集機能の追加

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -25,11 +25,10 @@ class PicturesController < ApplicationController
   end
 
   def edit
-
   end
 
   def update
-    if @picture.update
+    if @picture.update(picture_params)
       redirect_to picture_path(@picture.id), notice: "投稿内容を編集しました"
     else
       render :edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
   def new
     @user = User.new
   end
@@ -7,7 +8,6 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       redirect_to user_path(@user.id), notice: "ユーザーを新規登録しました"
-
     else
       flash.now[:danger] = "登録に失敗しました"
       render  :new
@@ -15,18 +15,23 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
-
   end
 
   def update
-
+    if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: "プロフィールを編集しました"
+    else
+      render :edit
+    end
   end
 
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :icon)
   end
